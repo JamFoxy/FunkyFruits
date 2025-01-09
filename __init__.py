fcccce51dedc56d6c342adf0b71ecc449aa15274
JamFoxy <94673529+JamFoxy@users.noreply.github.com>
2025-01-10 04:32:27 +0600

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_mail import Mail
+from .config import Config
+from .extensions import funkydb
+from .routes import main
+
+mail = Mail()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    mail.init_app(app)
+    funkydb.init_app(app)
+    app.register_blueprint(main)
+
+    return app
