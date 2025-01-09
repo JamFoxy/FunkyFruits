ebf91866a2cd1bb92f15d26bcf7830592d838220
JamFoxy <94673529+JamFoxy@users.noreply.github.com>
2025-01-10 04:49:54 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,7 @@
 import os
 
 class Config:
-    USER= os.environ.get('POSTGRES_USER', 'superdude')
-    PASSWORD= os.environ.get('POSTGRES_PASSWORD', '102010')
-    HOST= os.environ.get('POSTGRES_HOST', '127.0.0.1')
-    PORT= os.environ.get('POSTGRES_PORT', 5432)
-    DB= os.environ.get('POSTGRES_DB', 'funkydb')
-
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}'
     SECRET_KEY = 'wn1a3fncz2ayhapg2o1xznmuf'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
