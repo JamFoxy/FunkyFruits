d2b0998264eddbd71b7940f16baa4e753bde3cfa
JamFoxy <94673529+JamFoxy@users.noreply.github.com>
2025-01-10 07:39:16 +0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,13 @@ from flask_mail import Mail
 from .config import Config
 from .routes import main
 
+
 mail = Mail()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
     mail.init_app(app)
     app.register_blueprint(main)
 
     return app
-    
-app = create_app()
