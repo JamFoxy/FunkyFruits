0f9b3143fa66ac6bd1302fa531dd30ec7a9ce511
JamFoxy <94673529+JamFoxy@users.noreply.github.com>
2025-01-12 00:59:29 +0600

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_mail import Mail
+from .config import Config
+from .routes import main
+
+mail = Mail()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    mail.init_app(app)
+    app.register_blueprint(main)
+
+    return app
