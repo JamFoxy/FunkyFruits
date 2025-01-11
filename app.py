b4c40b8a79b4fedde8d90ec6691a3db62607d774
JamFoxy <94673529+JamFoxy@users.noreply.github.com>
2025-01-12 00:21:35 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-import os
-from app import create_app
+from app import create_app  # импортируем функцию создания приложения
 
-application = create_app()
+# создаем экземпляр приложения
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)  # запускаем приложение в режиме отладки
